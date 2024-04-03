89324af3d8835404addf525feeae6c6a993df26e
devSamuel <samueldflima@proton.me>
2024-04-02 22:37:04 -0300

    falta uns bagulho

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from graphs import *
 
 def get_degree(graph) -> dict:
@@ -18,17 +19,36 @@ def euler(graph: tuple):
 
     return 'Não é Euleriano!'
 
+def get_weights(graph: tuple):
+    n = len(graph)
+    weights = [random.randint(0, 10) for _ in range(n)]
+    return weights
+
+def get_path(graph: tuple):
+    weights = get_weights(graph)
+    graph_degrees = get_degree(graph)
+    count_vertex_degree_odd = sum(1 for degree in graph_degrees.values() if degree % 2 != 0)
+
+    if count_vertex_degree_odd > 2:
+        return 'Impossível de fazer o Caminho de Euler!'
+
+    return weights
+
 print('GRAFO 1')
 print(euler(graph1))
+print(get_path(graph1))
 print()
 
 print('GRAFO 2')
 print(euler(graph2))
+print(get_path(graph2))
 print()
 
 print('GRAFO 3')
 print(euler(graph3))
+print(get_path(graph3))
 print()
 
 print('GRAFO 4')
 print(euler(graph4))
+print(get_path(graph4))
