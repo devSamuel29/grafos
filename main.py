b00e19bd999544523c5d77ae5c734d0c4addc8d1
israelviana <rael232is@gmail.com>
2024-05-06 23:58:26 -0300

    adding comments in the code

--- main.py
+++ main.py
@@ -19,18 +19,28 @@ def dfs_recursive(graph: list, vertex: int, visited: set = None) -> list:
 
 def dfs_iterative(graph: list, initial_vertex: int) -> list:
     visited = set()
+
+    ## armazenando vertice que irao ser explorados
     stack = [(initial_vertex, None)]
+
+    ## armazena as arestas da arvore
     tree = []
 
+    ##enquanto houver vertices na pilha
     while stack:
+        ## u -> vertice atual; v -> pai do vertice atual
         u, v = stack.pop()
         if u not in visited:
             visited.add(u)
+
+            ##verifica se nao eh o primeiro da stack
             if v is not None:
                 tree.append((v, u))
 
             for edge in graph:
+                ##verifica se a aresta tem U como vertice
                 if u in edge:
+                    ## caso o edge[1] seja igual ao vertice atual (u), entao o vertice edge[0] eh vizinho do que estamos procurando
                     neighbor = edge[0] if edge[1] == u else edge[1]
                     if neighbor not in visited:
                         stack.append((neighbor, u))
